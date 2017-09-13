2458315d92462207563fb0a12f96fe194ab9bdc2
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-13 16:20:33 -0500

    testing

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+// var server = http.createServer((request, response) => {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+//     response.writeHead(200, { "Content-Type": "text/plain" });
+//     response.end("Hello World!");
 
+// });
+
+const server = http.createServer();
+server.on('request', (request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // At this point, we have the headers, method, url and body, and can now
+        // do whatever we need to in order to respond to this request.
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.end(`${headers} ${method} ${url} ${body}`);
+    });
 });
 
 var port = process.env.PORT || 1338;
