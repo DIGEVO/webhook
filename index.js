8c4b8b61fe170d40f3bfb53f9f21685c2c0b121e
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-13 23:44:40 -0500

    testing

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 'use strict';
 
 const http = require('http');
-const concat = require('concat-stream');
+// const concat = require('concat-stream');
 
 const server = http.createServer();
 
-const concatOnMessage = concat(gotMessage);
-const concatOnPause = concat(gotPause);
+// const concatOnMessage = concat(gotMessage);
+// const concatOnPause = concat(gotPause);
 
-function gotMessage(body) {
-    const responseBody = { body };
-    console.log(JSON.stringify(responseBody));
-}
+// function gotMessage(body) {
+//     const responseBody = { body };
+//     console.log(JSON.stringify(responseBody));
+// }
 
-function gotPause(body) {
-    const responseBody = { body };
-    console.log(JSON.stringify(responseBody));
-}
+// function gotPause(body) {
+//     const responseBody = { body };
+//     console.log(JSON.stringify(responseBody));
+// }
 
-function handleError(err) {
-    console.error(err);
-}
-
-server.on('request', (request, response) => {
-    request.on('error', handleError);
-
-    //request.pipe(request.url === '/message' ? concatOnMessage : concatOnPause);
-    request.pipe(concatOnMessage);
-
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(responseBody));
-});
+// function handleError(err) {
+//     console.error(err);
+// }
 
 // server.on('request', (request, response) => {
-//     const { headers, method, url } = request;
-//     let body = [];
-//     request.on('error', (err) => {
-//         console.error(err);
-//     }).on('data', (chunk) => {
-//         body.push(chunk);
-//     }).on('end', () => {
-//         body = Buffer.concat(body).toString();
+//     request.on('error', handleError);
 
-//         response.on('error', (err) => console.error(err));
+//     //request.pipe(request.url === '/message' ? concatOnMessage : concatOnPause);
+//     request.pipe(concatOnMessage);
 
-//         response.writeHead(200, { 'Content-Type': 'application/json' });
-
-//         const responseBody = { headers, method, url, body };
-
-//         console.log(JSON.stringify(responseBody));
-
-//         response.end(JSON.stringify(responseBody));
-//     });
+//     response.writeHead(200, { 'Content-Type': 'application/json' });
+//     response.end(JSON.stringify(responseBody));
 // });
+
+server.on('request', (request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        response.on('error', (err) => console.error(err));
+
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        const responseBody = { url, body };
+
+        console.log(JSON.stringify(responseBody));
+
+        response.end(JSON.stringify(responseBody));
+    });
+});
 
 
 
